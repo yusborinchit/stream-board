45b45df2ad889d3498668a829e1f7c50146466bd
Santiago Garcia <just.b000ringsh1t@gmail.com>
2024-08-30 18:28:15 -0300

    feat(frontend): :sparkles: add footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -45,53 +45,6 @@ export default function HomePage() {
             />
           </picture>
         </Container>
-        <Container as="section" className="mt-32">
-          <h2 className="text-3xl font-bold tracking-tight">
-            <span className="text-blue-600"># </span>
-            And How it Works?
-          </h2>
-          <div className="mt-12">
-            <StepCard>
-              <div>
-                <h4 className="text-xl font-semibold">
-                  1<span className="text-blue-600">. </span>
-                  Sign Up
-                </h4>
-                <p className="pb-12 text-neutral-500">
-                  Sign up with your Twitch account here.
-                </p>
-              </div>
-            </StepCard>
-            <StepCard>
-              <div>
-                <h4 className="text-xl font-semibold">
-                  2<span className="text-blue-600">. </span>
-                  Submit your media
-                </h4>
-                <p className="pb-12 text-neutral-500">
-                  Submit up to 20 videos and 20 sounds to our platform.
-                </p>
-              </div>
-            </StepCard>
-            <StepCard>
-              <div>
-                <h4 className="text-xl font-semibold">
-                  3<span className="text-blue-600">. </span>
-                  Customize your deck
-                </h4>
-                <p className="pb-12 text-neutral-500">
-                  Customize your deck with our drag-and-drop editor.
-                </p>
-              </div>
-            </StepCard>
-            <StepCard>
-              <h4 className="pb-4 text-5xl font-semibold">
-                4<span className="text-blue-600">. </span>
-                Enjoy! 🎉
-              </h4>
-            </StepCard>
-          </div>
-        </Container>
         <Container as="section" className="mt-32 grid gap-4 sm:grid-cols-2">
           <article className="rounded-md bg-gradient-to-t from-neutral-900/40 via-neutral-900/10 px-6 py-10 shadow">
             <h2 className="text-3xl font-bold tracking-tight">
@@ -122,9 +75,54 @@ export default function HomePage() {
             </p>
           </article>
         </Container>
+        <Container as="section" className="mt-32">
+          <h2 className="mb-8 text-center text-3xl font-bold tracking-tight">
+            <span className="text-blue-600"># </span>
+            And How it Works?
+          </h2>
+          <p className="mx-auto max-w-xl text-center text-neutral-500">
+            Follow the steps below to get started with Stream Board and level up
+            your stream.
+          </p>
+          <div className="mt-12">
+            <StepCard
+              step={1}
+              title="Sign Up"
+              description="Sign up with your Twitch account here."
+            />
+            <StepCard
+              step={2}
+              title="Submit your media"
+              description="Submit up to 20 videos and 20 sounds to our platform."
+            />
+            <StepCard
+              step={3}
+              title="Customize your deck"
+              description="Customize your deck with our drag-and-drop editor."
+            />
+            <StepCard
+              step={4}
+              title="Enjoy! 🎉"
+              description="Upload your media, customize your deck, and take charge of your streaming experience, all without spending a single cent."
+            />
+          </div>
+        </Container>
       </main>
-      <Container as="footer" className="mt-32">
-        footer
+      <Container
+        as="footer"
+        className="mt-32 flex items-center justify-between py-8 text-neutral-800"
+      >
+        <p>Copyright © 2024 Stream Board. All rights reserved.</p>
+        <p>
+          Made with ❤️ by{" "}
+          <a
+            href="https://github.com/yusborinchit"
+            target="_blank"
+            className="font-semibold underline"
+          >
+            @yusborinchit
+          </a>
+        </p>
       </Container>
       <Background />
     </>
--- frontend/src/components/landing/step-card.tsx
+++ frontend/src/components/landing/step-card.tsx
@@ -1,12 +1,24 @@
 interface Props {
-  children: React.ReactNode;
+  step: number;
+  title: string;
+  description: string;
+  className?: string;
 }
 
 export default function StepCard(props: Readonly<Props>) {
   return (
-    <article className="flex h-full gap-4">
-      <div className="relative ml-2 min-h-full w-[2.5px] bg-neutral-800 before:absolute before:left-1/2 before:top-0 before:size-4 before:-translate-x-1/2 before:rounded-full before:bg-neutral-800 before:outline before:outline-4 before:outline-neutral-950"></div>
-      {props.children}
+    <article className="group grid grid-cols-[1fr_14px_1fr] gap-4">
+      <div className="relative col-start-2 row-start-1 mx-auto w-[2px] bg-neutral-800 before:absolute before:left-1/2 before:top-0 before:size-3.5 before:-translate-x-1/2 before:rounded-full before:bg-neutral-800 before:outline before:outline-4 before:outline-neutral-950" />
+      <div className="row-start-1 group-odd:col-start-1 group-odd:text-end group-even:col-start-3 group-even:text-start">
+        <h4 className="text-xl font-semibold group-last:text-4xl">
+          {props.step}
+          <span className="text-blue-600">. </span>
+          {props.title}
+        </h4>
+        <p className="pb-8 text-neutral-500 group-last:pb-4">
+          {props.description}
+        </p>
+      </div>
     </article>
   );
 }
